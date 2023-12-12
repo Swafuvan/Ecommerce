542b67f6812a6c606606dbd2aea768b16f1696de
swafuvan <swafuvancp@gmail.com>
2023-12-12 21:04:34 +0530

    FourthCommit

--- app.js
+++ app.js
@@ -3,14 +3,87 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const session=require('express-session');
-const database=require('./model/database');
-const nocache=require('nocache');
-
+const session = require('express-session');
+const database = require('./model/database');
+const debug = require('debug')('project:server');
+const http = require('http');
+const nocache = require('nocache');
 const userRouter = require('./routes/user');
 const adminRouter = require('./routes/admin');
 
 const app = express();
+
+
+
+
+
+const port = normalizePort(process.env.PORT);
+app.set('port', port);
+
+
+const server = http.createServer(app);
+
+
+server.listen(port);
+console.log(`Server started http://Localhost:${port}`);
+
+server.on('error', onError);
+server.on('listening', onListening);
+
+
+
+function normalizePort(val) {
+  const port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+
+    return val;
+  }
+
+  if (port >= 0) {
+
+    return port;
+  }
+
+  return false;
+}
+
+
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  const bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+
+
+function onListening() {
+  const addr = server.address();
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  debug('Listening on ' + bind);
+}
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -24,10 +97,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.use(session({
-  secret:"colours123",
-  saveUninitialized:true,
-  cookie:{maxAge:1000*60*60},
-  resave:false
+  secret: "colours123",
+  saveUninitialized: true,
+  cookie: { maxAge: 1000 * 60 * 60 },
+  resave: false
 }))
 
 app.use(nocache());
@@ -36,12 +109,12 @@ app.use('/', userRouter);
 app.use('/admin', adminRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
